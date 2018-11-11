4f321aad228b87df35f387d1284d41d3eb2691ea
Authorty <itismejody@GMAIL.COM>
2018-11-10 19:44:35 -0600

    add

--- TheNorthernHandyManReDone11_3_18/Controllers/HomeController.cs
+++ TheNorthernHandyManReDone11_3_18/Controllers/HomeController.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
+using System.Web.Mail;
 using System.Web.Mvc;
+using TheNorthernHandyManReDone11_3_18.Models;
 
 namespace TheNorthernHandyManReDone11_3_18.Controllers
 {
@@ -19,11 +24,227 @@ namespace TheNorthernHandyManReDone11_3_18.Controllers
 
             return View();
         }
-
-        public ActionResult Contact()
+        public ActionResult SendEmail(string Name, string Phone, string Message, string Email, string Subject)
         {
-            ViewBag.Message = "Your contact page.";
 
+            var passWord = "northernhandyman";
+            var email = new MailAddress("thenorthernhandyman@gmail.com", "Jeff");
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(email.Address, passWord)
+            };
+
+            using (var mess = new System.Net.Mail.MailMessage(email, email)
+            {
+                Subject = Subject,
+                Body = "From: " + Name + "\n Phone Number: " + Phone + "\n Job Description: " + Message + "\n Email: " + Email
+            })
+            {
+                smtp.Send(mess);
+            }
+
+
+
+
+            //System.Web.Mail.SmtpMail.SmtpServer = "relay-hosting.secureserver.net";
+            //SmtpMail.Send(emailmessage);
+            MessageBox("Email sent successfully!");
+
+
+            return new EmptyResult();
+        }
+
+        public ActionResult Contact(ContactEmailModels e)
+        {
+            string ValidationMessage = String.Empty;
+
+            ViewBag.Name = String.Empty;
+            ViewBag.Email = String.Empty;
+            ViewBag.Phone = String.Empty;
+            ViewBag.Message = String.Empty;
+
+
+            if (!String.IsNullOrEmpty(e.Name) && !String.IsNullOrEmpty(e.Message) && !String.IsNullOrEmpty(e.Phone) && !String.IsNullOrEmpty(e.Email))
+            {
+                var emailmessage = new System.Web.Mail.MailMessage()
+                {
+                    Subject = e.Name,
+                    Body = "From: " + e.Name + "\n Phone Number: " + e.Phone + "\n Job Description: " + e.Message + "\n Email: " + e.Email,
+                    From = "thenorthernhandyman@thenorthernhandyman.org",
+                    To = "thenorthernhandyman@gmail.org",
+                    BodyFormat = MailFormat.Text,
+                    Priority = System.Web.Mail.MailPriority.High
+                };
+                var passWord = "northernhandyman";
+                var email = new MailAddress("thenorthernhandyman@gmail.com", "Jeff");
+
+                var smtp = new SmtpClient
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential(email.Address, passWord)
+                };
+
+                using (var mess = new System.Net.Mail.MailMessage(email, email)
+                {
+                    Subject = e.Name,
+                    Body = "From: " + e.Name + "\n Phone Number: " + e.Phone + "\n Job Description: " + e.Message + "\n Email: " + e.Email
+                })
+                {
+                    smtp.Send(mess);
+                }
+
+
+
+
+                //System.Web.Mail.SmtpMail.SmtpServer = "relay-hosting.secureserver.net";
+                //SmtpMail.Send(emailmessage);
+                MessageBox("Email sent successfully!");
+                //return RedirectToAction("Index", "Home", null);
+
+            }
+
+            return View(e);
+        }
+        public EmptyResult MessageBox(string s)
+        {
+            Response.Write("<script type=\"text/javascript\">alert('" + s + "');</script>");
+            return null;
+        }
+
+
+        public ActionResult Services()
+        {
+            return View();
+        }
+        public ActionResult SkidSteerWork()
+        {
+            return View();
+        }
+        public ActionResult Photos()
+        {
+
+            var images = new List<ImagesViewModel>();
+
+            DirectoryInfo imageDir = null;
+            FileInfo[] imageFiles = null;
+
+            string dirPath = @"/img/";
+            imageDir = new DirectoryInfo(Server.MapPath(dirPath));
+
+            var imageChildren = imageDir.GetDirectories();
+
+            foreach (var imageDirectory in imageChildren)
+            {
+                
+                var DirectoryName = imageDirectory.Name;
+                if (DirectoryName != "clients" && DirectoryName != "backgrounds")
+                {
+                    var imagefilePath = "";
+
+                    foreach (var image in imageDirectory.GetFiles())
+                    {
+                        imagefilePath = @"/img/" + DirectoryName + "/" + image.Name;
+                        images.Add(new ImagesViewModel { Group = DirectoryName, Image = imagefilePath });
+                    }
+                }
+
+
+                
+            }
+            //imageFiles = imageDir.GetFiles();
+            
+
+
+            ViewBag.Images = images;
+
+
+
+
+            return View("Images");
+        }
+        public ActionResult Videos()
+        {
+            return View();
+        }
+        public ActionResult Brush_Remvoal_Hogging()
+        {
+            return View();
+        }
+        public ActionResult Limb_Removal()
+        {
+            return View();
+        }
+        public ActionResult Mowing()
+        {
+            return View();
+        }
+        public ActionResult Projects()
+        {
+            return View();
+        }
+        public ActionResult Stump_Grinding()
+        {
+            return View();
+        }
+        public ActionResult Tree_Removal()
+        {
+            return View();
+        }
+        public ActionResult Trimming()
+        {
+            return View();
+        }
+        public ActionResult AllPhotos()
+        {
+            return View();
+        }
+        public ActionResult Tree_Stump_Removal_Index()
+        {
+            return View();
+        }
+        public ActionResult Landscape_Lawncare_Index(int? carouselId)
+        {
+            if (carouselId == null)
+            {
+                carouselId = 1;
+            }
+
+            ViewBag.CarouselId = carouselId;
+
+            return View();
+        }
+        public ActionResult Winter_Services_Index()
+        {
+            return View();
+        }
+        public ActionResult LeafRemoval()
+        {
+            return View();
+        }
+        public ActionResult AllPhotosPg6()
+        {
+            return View();
+        }
+        public ActionResult AllPhotosPg7()
+        {
+            return View();
+        }
+        public ActionResult AllPhotosPg8()
+        {
+            return View();
+        }
+        public ActionResult Portfolio_Modal1()
+        {
             return View();
         }
     }
---
+++ TheNorthernHandyManReDone11_3_18/Models/ImagesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheNorthernHandyManReDone11_3_18.Models
+{
+    public class ImagesViewModel
+    {
+        public string Group { get; set; }
+        public string Image { get; set; }
+
+
+    }
+}
